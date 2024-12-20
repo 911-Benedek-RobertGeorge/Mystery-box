0df9694cd40060adc2e6c6aa933b6796aee1c301
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-12-21 01:08:25 +0200

    fix: vercel env vars

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -2,42 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig(({ mode }) => {
-    const isProduction = mode === 'production'
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3000,
 
-    return {
-        plugins: [react()],
-        server: {
-            port: 3000,
-            watch: {
-                usePolling: true,
-            },
+        watch: {
+            usePolling: true,
         },
-        resolve: {
-            alias: {
-                '@libs': path.resolve(__dirname, './src/libs'),
-                '@assets': path.resolve(__dirname, './src/assets'),
-                '@src': path.resolve(__dirname, './src'),
-            },
+    },
+    resolve: {
+        alias: {
+            '@libs': path.resolve(__dirname, './src/libs'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@src': path.resolve(__dirname, './src'),
         },
-        define: isProduction
-            ? {
-                  'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
-                      process.env.VITE_ENV_BACKEND_API
-                  ),
-                  'import.meta.env.VITE_ENV_NETWORK': JSON.stringify(
-                      process.env.VITE_ENV_NETWORK
-                  ),
-                  'import.meta.env.VITE_ENV_SOLANA_NETWORK_RPC': JSON.stringify(
-                      process.env.VITE_ENV_SOLANA_NETWORK_RPC
-                  ),
-                  'import.meta.env.VITE_ENV_BACKEND_URL': JSON.stringify(
-                      process.env.VITE_ENV_BACKEND_URL
-                  ),
-                  'import.meta.env.VITE_ENV_BACKEND_PUBLIC_KEY': JSON.stringify(
-                      process.env.VITE_ENV_BACKEND_PUBLIC_KEY
-                  ),
-              }
-            : {},
-    }
+    },
+    define: {
+        'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
+            process.env.VITE_ENV_BACKEND_API
+        ),
+        'import.meta.env.VITE_ENV_NETWORK': JSON.stringify(
+            process.env.VITE_ENV_NETWORK
+        ),
+        'import.meta.env.VITE_ENV_SOLANA_NETWORK_RPC': JSON.stringify(
+            process.env.VITE_ENV_SOLANA_NETWORK_RPC
+        ),
+        'import.meta.env.VITE_ENV_BACKEND_URL': JSON.stringify(
+            process.env.VITE_ENV_BACKEND_URL
+        ),
+        'import.meta.env.VITE_ENV_BACKEND_PUBLIC_KEY': JSON.stringify(
+            process.env.VITE_ENV_BACKEND_PUBLIC_KEY
+        ),
+    },
 })
