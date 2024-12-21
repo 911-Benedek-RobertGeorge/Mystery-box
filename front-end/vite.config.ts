46499c8f320bedbf546df5b484a9d24fe8418d82
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-12-21 15:07:17 +0200

    Update vite.config.ts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -24,38 +24,38 @@ export default defineConfig({
             '@': '/src',
         },
     },
-    // define: {
-    //     'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
-    //         process.env.VITE_ENV_BACKEND_API
-    //     ),
-    //     'import.meta.env.VITE_ENV_NETWORK': JSON.stringify(
-    //         process.env.VITE_ENV_NETWORK
-    //     ),
-    //     'import.meta.env.VITE_ENV_SOLANA_NETWORK_RPC': JSON.stringify(
-    //         process.env.VITE_ENV_SOLANA_NETWORK_RPC
-    //     ),
-    //     'import.meta.env.VITE_ENV_BACKEND_URL': JSON.stringify(
-    //         process.env.VITE_ENV_BACKEND_URL
-    //     ),
-    //     'import.meta.env.VITE_ENV_BACKEND_PUBLIC_KEY': JSON.stringify(
-    //         process.env.VITE_ENV_BACKEND_PUBLIC_KEY
-    //     ),
-    // },
-
     define: {
-        'import.meta.env': {
-            VITE_ENV_NETWORK: JSON.stringify('mainnet-beta'),
-            VITE_ENV_SOLANA_NETWORK_RPC: JSON.stringify(
-                'https://solana-mainnet.g.alchemy.com/v2/nL4aWYneHAajUr9tLDp4asFLLGnAd45H'
-            ),
-            VITE_ENV_BACKEND_URL: JSON.stringify(
-                'https://ejacdvrot9.execute-api.eu-central-1.amazonaws.com/api'
-            ),
-            VITE_ENV_BACKEND_PUBLIC_KEY: JSON.stringify(
-                'GQqFaWvE2RAQc7V6aTrpWzKmJBNi8qf3G6SwWHL5Yttq'
-            ),
-        },
+        'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
+            process.env.VITE_ENV_BACKEND_API
+        ),
+        'import.meta.env.VITE_ENV_NETWORK': JSON.stringify(
+            process.env.VITE_ENV_NETWORK
+        ),
+        'import.meta.env.VITE_ENV_SOLANA_NETWORK_RPC': JSON.stringify(
+            process.env.VITE_ENV_SOLANA_NETWORK_RPC
+        ),
+        'import.meta.env.VITE_ENV_BACKEND_URL': JSON.stringify(
+            process.env.VITE_ENV_BACKEND_URL
+        ),
+        'import.meta.env.VITE_ENV_BACKEND_PUBLIC_KEY': JSON.stringify(
+            process.env.VITE_ENV_BACKEND_PUBLIC_KEY
+        ),
     },
+
+    // define: {
+    //     'import.meta.env': {
+    //         VITE_ENV_NETWORK: JSON.stringify('mainnet-beta'),
+    //         VITE_ENV_SOLANA_NETWORK_RPC: JSON.stringify(
+    //             'https://solana-mainnet.g.alchemy.com/v2/nL4aWYneHAajUr9tLDp4asFLLGnAd45H'
+    //         ),
+    //         VITE_ENV_BACKEND_URL: JSON.stringify(
+    //             'https://ejacdvrot9.execute-api.eu-central-1.amazonaws.com/api'
+    //         ),
+    //         VITE_ENV_BACKEND_PUBLIC_KEY: JSON.stringify(
+    //             process.env.VITE_ENV_BACKEND_PUBLIC_KEY
+    //         ),
+    //     },
+    // },
     build: {
         sourcemap: true, // Useful for debugging
         outDir: 'dist', // Build output directory
