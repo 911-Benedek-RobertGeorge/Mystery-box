8ec4c12dd15f439e10c3fa0c2591d4c9aa854e4f
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-12-21 15:13:10 +0200

    fix: env vars vercel

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,29 +1,12 @@
-import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import svgrPlugin from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
 
 export default defineConfig({
-    plugins: [
-        react(), // React plugin for Vite
-        tsconfigPaths(), // TS path alias support
-    ],
-    server: {
-        port: 3000, // Dev server port
-        open: true, // Open browser on server start
-    },
-    // resolve: {
-    //     alias: {
-    //         '@libs': path.resolve(__dirname, './src/libs'),
-    //         '@assets': path.resolve(__dirname, './src/assets'),
-    //         '@src': path.resolve(__dirname, './src'),
-    //     }}
-    resolve: {
-        alias: {
-            // Path alias
-            '@': '/src',
-        },
-    },
     define: {
         'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
             process.env.VITE_ENV_BACKEND_API
@@ -41,30 +24,34 @@ export default defineConfig({
             process.env.VITE_ENV_BACKEND_PUBLIC_KEY
         ),
     },
-
-    // define: {
-    //     'import.meta.env': {
-    //         VITE_ENV_NETWORK: JSON.stringify('mainnet-beta'),
-    //         VITE_ENV_SOLANA_NETWORK_RPC: JSON.stringify(
-    //             'https://solana-mainnet.g.alchemy.com/v2/nL4aWYneHAajUr9tLDp4asFLLGnAd45H'
-    //         ),
-    //         VITE_ENV_BACKEND_URL: JSON.stringify(
-    //             'https://ejacdvrot9.execute-api.eu-central-1.amazonaws.com/api'
-    //         ),
-    //         VITE_ENV_BACKEND_PUBLIC_KEY: JSON.stringify(
-    //             process.env.VITE_ENV_BACKEND_PUBLIC_KEY
-    //         ),
-    //     },
-    // },
-    build: {
-        sourcemap: true, // Useful for debugging
-        outDir: 'dist', // Build output directory
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['react', 'react-dom'], // Separate vendor code
-                },
-            },
+    server: {
+        port: Number(process.env.PORT) || 3000,
+        strictPort: true,
+        host: true,
+        watch: {
+            usePolling: false,
+            useFsEvents: false,
         },
+        hmr: {
+            overlay: false,
+        },
+    },
+    plugins: [
+        react(),
+
+        basicSsl(),
+        tsconfigPaths(),
+        svgrPlugin(),
+        nodePolyfills({
+            globals: { Buffer: true, global: true, process: true },
+        }),
+    ],
+    build: {
+        outDir: 'dist',
+    },
+    preview: {
+        port: 3333,
+        host: 'localhost',
+        strictPort: true,
     },
 })
