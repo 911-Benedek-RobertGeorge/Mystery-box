27ec72fb4b0bbfe06cb801aecedeb00aa5f53424
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-12-21 02:00:10 +0200

    fix: wip vercel env vars

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -3,21 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-
-        watch: {
-            usePolling: true,
-        },
-    },
-    resolve: {
-        alias: {
-            '@libs': path.resolve(__dirname, './src/libs'),
-            '@assets': path.resolve(__dirname, './src/assets'),
-            '@src': path.resolve(__dirname, './src'),
-        },
-    },
     define: {
         'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
             process.env.VITE_ENV_BACKEND_API
@@ -34,5 +19,28 @@ export default defineConfig({
         'import.meta.env.VITE_ENV_BACKEND_PUBLIC_KEY': JSON.stringify(
             process.env.VITE_ENV_BACKEND_PUBLIC_KEY
         ),
+    },
+    plugins: [react()],
+    server: {
+        port: 3000,
+        strictPort: true,
+        host: true,
+        watch: {
+            usePolling: false,
+            useFsEvents: false,
+        },
+        hmr: {
+            overlay: false,
+        },
+    },
+    resolve: {
+        alias: {
+            '@libs': path.resolve(__dirname, './src/libs'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@src': path.resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        outDir: 'build',
     },
 })
