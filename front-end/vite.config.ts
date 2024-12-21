53c4d092afb4a3ded7a7ba9c22e8c8394714e50c
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-12-21 15:01:09 +0200

    fix: vercel env vars

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
 export default defineConfig({
+    plugins: [
+        react(), // React plugin for Vite
+        tsconfigPaths(), // TS path alias support
+    ],
+    server: {
+        port: 3000, // Dev server port
+        open: true, // Open browser on server start
+    },
+    // resolve: {
+    //     alias: {
+    //         '@libs': path.resolve(__dirname, './src/libs'),
+    //         '@assets': path.resolve(__dirname, './src/assets'),
+    //         '@src': path.resolve(__dirname, './src'),
+    //     }}
+    resolve: {
+        alias: {
+            // Path alias
+            '@': '/src',
+        },
+    },
     // define: {
     //     'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
     //         process.env.VITE_ENV_BACKEND_API
@@ -20,24 +41,30 @@ export default defineConfig({
     //         process.env.VITE_ENV_BACKEND_PUBLIC_KEY
     //     ),
     // },
-    plugins: [react()],
-    server: {
-        port: 3000,
-        strictPort: true,
-        host: true,
-        watch: {
-            usePolling: false,
-            useFsEvents: false,
-        },
-        hmr: {
-            overlay: false,
+
+    define: {
+        'import.meta.env': {
+            VITE_ENV_NETWORK: JSON.stringify('mainnet-beta'),
+            VITE_ENV_SOLANA_NETWORK_RPC: JSON.stringify(
+                'https://solana-mainnet.g.alchemy.com/v2/nL4aWYneHAajUr9tLDp4asFLLGnAd45H'
+            ),
+            VITE_ENV_BACKEND_URL: JSON.stringify(
+                'https://ejacdvrot9.execute-api.eu-central-1.amazonaws.com/api'
+            ),
+            VITE_ENV_BACKEND_PUBLIC_KEY: JSON.stringify(
+                'GQqFaWvE2RAQc7V6aTrpWzKmJBNi8qf3G6SwWHL5Yttq'
+            ),
         },
     },
-    resolve: {
-        alias: {
-            '@libs': path.resolve(__dirname, './src/libs'),
-            '@assets': path.resolve(__dirname, './src/assets'),
-            '@src': path.resolve(__dirname, './src'),
+    build: {
+        sourcemap: true, // Useful for debugging
+        outDir: 'dist', // Build output directory
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'], // Separate vendor code
+                },
+            },
         },
     },
 })
