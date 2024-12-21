125ca3597bd9996bf9920b3aec4cd3dfa34dc31b
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-12-21 02:04:37 +0200

    Update vite.config.ts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -3,23 +3,23 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-    define: {
-        'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
-            process.env.VITE_ENV_BACKEND_API
-        ),
-        'import.meta.env.VITE_ENV_NETWORK': JSON.stringify(
-            process.env.VITE_ENV_NETWORK
-        ),
-        'import.meta.env.VITE_ENV_SOLANA_NETWORK_RPC': JSON.stringify(
-            process.env.VITE_ENV_SOLANA_NETWORK_RPC
-        ),
-        'import.meta.env.VITE_ENV_BACKEND_URL': JSON.stringify(
-            process.env.VITE_ENV_BACKEND_URL
-        ),
-        'import.meta.env.VITE_ENV_BACKEND_PUBLIC_KEY': JSON.stringify(
-            process.env.VITE_ENV_BACKEND_PUBLIC_KEY
-        ),
-    },
+    // define: {
+    //     'import.meta.env.VITE_ENV_BACKEND_API': JSON.stringify(
+    //         process.env.VITE_ENV_BACKEND_API
+    //     ),
+    //     'import.meta.env.VITE_ENV_NETWORK': JSON.stringify(
+    //         process.env.VITE_ENV_NETWORK
+    //     ),
+    //     'import.meta.env.VITE_ENV_SOLANA_NETWORK_RPC': JSON.stringify(
+    //         process.env.VITE_ENV_SOLANA_NETWORK_RPC
+    //     ),
+    //     'import.meta.env.VITE_ENV_BACKEND_URL': JSON.stringify(
+    //         process.env.VITE_ENV_BACKEND_URL
+    //     ),
+    //     'import.meta.env.VITE_ENV_BACKEND_PUBLIC_KEY': JSON.stringify(
+    //         process.env.VITE_ENV_BACKEND_PUBLIC_KEY
+    //     ),
+    // },
     plugins: [react()],
     server: {
         port: 3000,
