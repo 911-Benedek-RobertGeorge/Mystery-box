b0eff318c416ce859ce182a46845d99f8bd425e9
giurgiur99 <giurgiur99@gmail.com>
2025-01-03 12:04:23 +0200

    new api url

--- front-end/src/libs/config.ts
+++ front-end/src/libs/config.ts
@@ -1,6 +1,6 @@
 export const VITE_ENV_NETWORK = 'mainnet-beta'
 export const VITE_ENV_SOLANA_NETWORK_RPC = 'https://solana-rpc.publicnode.com/'
 export const VITE_ENV_BACKEND_URL =
-    'https://ejacdvrot9.execute-api.eu-central-1.amazonaws.com/api'
+    'https://memebox-sol-be-production.up.railway.app/api'
 export const VITE_ENV_BACKEND_PUBLIC_KEY =
     'GQqFaWvE2RAQc7V6aTrpWzKmJBNi8qf3G6SwWHL5Yttq'
